75ecc341844438acf8b72a0909b95eeb3f5307f9
stipe.katusic <stipe.katusic@king-ict.eu>
2023-07-13 23:50:34 +0200

    Get All Admins

--- KingAkademija2023/src/Api/Controllers/UserController.cs
+++ KingAkademija2023/src/Api/Controllers/UserController.cs
@@ -1,22 +1,30 @@
 ﻿using Api.Common;
 using Application.Commands;
 using Application.Common.Dtos;
+using Application.Queries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    public class UserController : BaseApiController
-    {
-        [HttpPost("save-role")]
-        public async Task<IActionResult> SaveRole([FromBody] RoleDto roleDto)
-        {
-            return Ok(await Mediator.Send(new SaveRoleCommand() { RoleDto = roleDto }));
-        }
+	public class UserController : BaseApiController
+	{
+		[HttpPost("save-role")]
+		public async Task<IActionResult> SaveRole([FromBody] RoleDto roleDto)
+		{
+			return Ok(await Mediator.Send(new SaveRoleCommand() { RoleDto = roleDto }));
+		}
 
-        [HttpPost("save-user")]
-        public async Task<IActionResult> SaveUser([FromBody] UserDto userDto)
-        {
-            return Ok(await Mediator.Send(new SaveUserCommand() { UserDto = userDto }));
-        }
-    }
+		[HttpPost("save-user")]
+		public async Task<IActionResult> SaveUser([FromBody] UserDto userDto)
+		{
+			return Ok(await Mediator.Send(new SaveUserCommand() { UserDto = userDto }));
+		}
+
+		[HttpGet("get-admins")]
+		public async Task<IActionResult> GetAllAdmins()
+		{
+			return Ok(await Mediator.Send(new GetAllAdminsQuery() { }));
+		}
+
+	}
 }
